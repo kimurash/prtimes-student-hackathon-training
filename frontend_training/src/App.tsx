3319a99f60bfb507a76d719e86bdd424cb3a2aac
shunsei <k.shunsei1227@gmail.com>
2025-02-14 01:10:08 +0900

    Setup tailwind css

--- frontend_training/src/App.tsx
+++ frontend_training/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>TODOアプリ</h1>
+      <h1 className="text-3xl">TODOアプリ</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
