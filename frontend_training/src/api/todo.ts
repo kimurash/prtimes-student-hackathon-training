790f08e623a666f3d5987e35022d33af842331db
shunsei <k.shunsei1227@gmail.com>
2025-02-24 21:02:02 +0900

    Add todo api module

---
+++ frontend_training/src/api/todo.ts
@@ -0,0 +1,110 @@
+const ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+
+export interface TodoRequest {
+  title: string;
+  status: "pending" | "active" | "completed";
+}
+
+export interface TodoResponse {
+  status: string;
+  data: {
+    id: number;
+    title: string;
+    status: "pending" | "active" | "completed";
+  }[];
+}
+
+export const fetchTodos = async () => {
+  // prettier-ignore
+  const response = await fetch(
+    `${ENDPOINT}/todos`,
+    {
+      method: "GET"
+    }
+  );
+  if (!response.ok) {
+    return [];
+  }
+
+  const resBody: TodoResponse = await response.json();
+  const todoDataList = resBody.data;
+  // prettier-ignore
+  const todos = todoDataList.map(
+    (todoData) => {
+      return {
+        ...todoData,
+        isEditing: false,
+      }
+    }
+  );
+
+  return todos;
+};
+
+export const fetchTodo = async (todoId: number) => {
+  // prettier-ignore
+  const response = await fetch(
+    `${ENDPOINT}/todos/${todoId}`,
+    {
+      method: "GET"
+    }
+  );
+  if (!response.ok) {
+    return null;
+  }
+  const resBody: TodoResponse = await response.json();
+
+  const todos = resBody.data;
+  if (todos.length === 0) {
+    return null;
+  } else {
+    return {
+      ...todos[0],
+      isEditing: false,
+    };
+  }
+};
+
+export const createTodo = async (todo: TodoRequest) => {
+  // prettier-ignore
+  const response = await fetch(
+    `${ENDPOINT}/todos`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    }
+  );
+
+  return response.ok;
+};
+
+export const updateTodo = async (todoId: number, todo: TodoRequest) => {
+  // prettier-ignore
+  const response = await fetch(
+    `${ENDPOINT}/todos?id=${todoId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    }
+  );
+
+  return response.ok;
+};
+
+export const deleteTodo = async (todoId: number) => {
+  // prettier-ignore
+  const response = await fetch(
+    `${ENDPOINT}/todos?id=${todoId}`,
+    {
+      method: "DELETE",
+    }
+  );
+
+  return response.ok;
+};
